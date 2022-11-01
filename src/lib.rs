930803ad673a9c24449bea3d9a8295e9283223f9
William Trung <tngo@tibco.com>
2022-11-01 11:01:13 +0700

    Add the first command to greeting

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use anyhow::anyhow;
-use serenity::async_trait;
+use serenity::model::application::interaction::Interaction;
+use serenity::model::prelude::interaction::InteractionResponseType;
+use serenity::{async_trait, model::prelude::GuildId};
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
@@ -10,16 +12,43 @@ struct Bot;
 
 #[async_trait]
 impl EventHandler for Bot {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-                error!("Error sending message: {:?}", e);
-            }
-        }
+    async fn ready(&self, _ctx: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+        let guild_id = GuildId(992199576842928138);
+
+        let commands = 
+            GuildId::set_application_commands(
+                &guild_id,
+                &_ctx.http,
+                |commands| {
+                    commands.create_application_command(|commands| {
+                        commands.name("hello").description("Say hello")
+                    })
+                }
+            )
+            .await
+            .unwrap();
     }
 
-    async fn ready(&self, _: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.name);
+    async fn interaction_create(&self, _ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let response_content = match command.data.name.as_str() {
+                "hello" => "hello".to_owned(),
+                command => unreachable!("Unknown command: {}", command),
+            };
+
+            let create_interaction_response =
+                command.create_interaction_response(&_ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| {
+                            message.content(response_content)
+                        })
+                });
+            if let Err(why) = create_interaction_response.await {
+                eprint!("Cannot respond to slask command: {}", why);
+            }
+        }
     }
 }
 
